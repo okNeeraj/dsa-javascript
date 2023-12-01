6c50d6df9e9ca132ec87eac22243f2742e61b0ff
Neeraj Singh <neerajonetwo@gmail.com>
2023-12-01 20:54:45 +0530

    frequency patter resolved

--- src/Practice/Frequency.js
+++ src/Practice/Frequency.js
@@ -20,5 +20,20 @@ let charCount = characterCount(string);
 console.log(`Character count of "${string}" >> `, charCount)
 
 export const sameFrequency = (arr1, arr2) => {
-
+	// Check, if both array has same length, if not return false
+	if (arr1.length !== arr2.length) return false;
+	// Loop throw arr1 and compare each index with arr2's index,
+	for (let i = 0; i < arr1.length; i++) {
+		const correctIndex = arr2.indexOf(arr1[i] ** 2);
+		if (correctIndex === -1) return false;
+		console.log(arr2)
+		arr2.splice(correctIndex, 1)
+	}
+	return true;
 }
+
+const arr1 = [1, 2, 3]
+const arr2 = [1, 4, 9]
+const isFrequencySquare = sameFrequency(arr1, arr2);
+console.log(isFrequencySquare)
+
